9b29351094267ecb4139d6fcd9072d4ef2d8804b
Darren Tsung <darren@onesignal.com>
2019-01-15 11:37:29 -0800

    Fix build-for-ubuntu to always build a new binary

--- build-for-ubuntu.sh
+++ build-for-ubuntu.sh
@@ -2,6 +2,5 @@
 
 mkdir -p output
 
-# docker build -t rspec-timing-splitter .
-# docker run --rm -v ${PWD}/output:/code/output rspec-timing-splitter "mv /code/target/release/rspec-timing-tool /code/output/rspec-timing-tool"
+docker build -t rspec-timing-splitter .
 docker run --rm -v ${PWD}/output:/code/output rspec-timing-splitter bash -c "mv target/release/rspec-timing-tool output"
